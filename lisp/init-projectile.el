4a73faa46a753c6f61f1af339c62dd24538a3dc1
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-04-15 01:57:35 +0200

    remove my on projectile fn

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -50,40 +50,6 @@
 	(progn
 		(mikus-leader "I" 'imenu-anywhere)))
 
-;; Find files source
-(after-load 'helm-projectile
-	(defun mikus-helm-projectile-find-file ()
-		"Find file at point based on context."
-		(interactive)
-		(let* ((project-root (projectile-project-root))
-					 (project-files (projectile-current-project-files))
-					 (files (projectile-select-files project-files)))
-			(if (= (length files) 1)
-					(find-file (expand-file-name (car files) (projectile-project-root)))
-				(helm :sources (helm-build-sync-source "Projectile files"
-												 :candidates (if (> (length files) 1)
-																				 (helm-projectile--files-display-real files project-root)
-																			 (helm-projectile--files-display-real project-files project-root))
-												 :fuzzy-match helm-projectile-fuzzy-match
-												 :action-transformer 'helm-find-files-action-transformer
-												 :keymap helm-projectile-find-file-map
-												 :help-message helm-ff-help-message
-												 :mode-line helm-read-file-name-mode-line-string
-												 :action helm-projectile-file-actions
-												 :persistent-action #'helm-projectile-file-persistent-action
-												 ;; :match-part (lambda (c) (helm-basename c))
-												 :persistent-help "Preview file")
-							:buffer "*helm projectile*"
-							:truncate-lines helm-projectile-truncate-lines
-							:prompt (projectile-prepend-project-name "Find file: ")))))
-
-	(setq projectile-switch-project-action 'mikus-helm-projectile-find-file)
-
-	(general-define-key
-	 :keymaps 'projectile-command-map
-	 "f" 'mikus-helm-projectile-find-file)
-	)
-
 ;; Tags
 (after-load 'helm-projectile
 
