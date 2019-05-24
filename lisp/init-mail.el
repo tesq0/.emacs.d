952ed8cd816f9d6156543da256d897a1b46da14b
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-05-25 01:09:07 +0200

    modify init mail

--- lisp/init-mail.el
+++ lisp/init-mail.el
@@ -1,11 +1,9 @@
-(use-package mu4e
-	:ensure nil
-	:init
-	(progn
-		(setq mail-user-agent 'mu4e-user-agent
-					mu4e-maildir (expand-file-name "~/.mail")
-					mu4e-get-mail-command "mbsync gmail"
-					mu4e-change-filenames-when-moving t)))
+(require 'mu4e)
+(setq mail-user-agent 'mu4e-user-agent
+			mu4e-maildir (expand-file-name "~/.mail/gmail")
+			mu4e-get-mail-command "mbsync gmail"
+			mu4e-change-filenames-when-moving t
+			mu4e-sent-folder "[]")
 
 (provide 'init-mail)
 
