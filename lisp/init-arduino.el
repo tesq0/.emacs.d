ab542aff5614d81deefc2af710947efad58d5104
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-07-20 10:32:52 +0200

    change some stuff in arduino mode

--- lisp/init-arduino.el
+++ lisp/init-arduino.el
@@ -1,14 +1,9 @@
-(defun setup-arduino ()
-	(setq-local company-backends '(company-files (company-dabbrev-code :with company-arduino company-yasnippet company-keywords) )))
-
 (use-package arduino-mode
 	:ensure t)
 
 (use-package company-arduino
-	:after arduino-mode
-	:hook (arduino-mode . setup-arduino)
-	:init
-	(setq irony-disabled-modes '(arduino-mode)))
+	:ensure t
+	:after arduino-mode)
 
 (provide 'init-arduino)
 
