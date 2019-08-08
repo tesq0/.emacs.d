0f1273a6e9b3a0a549775bf72f078717cd7793e6
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-08-08 12:04:45 +0200

    fix java setup

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -1,18 +1,27 @@
-(defun init-Java ()
+(defun lsp-java-boot--find-tools-jar-nixos (orig-fun &rest args)
+	"Find the java tools.jar on nixos."
+	(let* ((command "nix-env -q --out-path openjdk | awk '{print $2}'")
+				 (raw-output (shell-command-to-string command))
+				 (command-output (s-chomp raw-output))
+				 (full-path (concat command-output "/lib/openjdk/lib/tools.jar"))
+				 (lsp-java-boot-java-tools-jar full-path))
+		(message "java jar %s" lsp-java-boot-java-tools-jar)
+		(apply orig-fun)))
 
-	;; lsp setup
+(defun setup-Java ()
+	"Setup before lsp-java mode."
 	(setq-local company-manual-completion-fn #'company-lsp)
-	(setq lsp-inhibit-message t)
-	(setq lsp-eldoc-render-all t)
-	(setq lsp-highlight-symbol-at-point nil)
-
-	;; rest
 	(flycheck-mode))
 
-(use-package lsp-mode
-  :ensure t)
+(defun init-Java ()
+	"Some setup after lsp-java init."
+	(advice-add 'lsp-java-boot--find-tools-jar :around #'lsp-java-boot--find-tools-jar-nixos)
+	;; (advice-remove 'lsp-java-boot--find-tools-jar #'lsp-java-boot--find-tools-jar-nixos)
+	(setq lsp-inhibit-message t
+				lsp-eldoc-render-all t
+				lsp-highlight-symbol-at-point nil))
 
-(use-package treemacs
+(use-package lsp-mode
   :ensure t)
 
 (use-package groovy-mode
@@ -32,9 +41,11 @@
 
 (use-package lsp-java
   :ensure t
+	:demand t
   :init
-	(add-hook 'java-mode-hook 'lsp-java-enable)
-	(add-hook 'java-mode-hook 'init-Java))
+	(init-Java)
+	(add-hook 'java-mode-hook 'lsp)
+	(add-hook 'java-mode-hook 'setup-Java))
 
 (use-package dap-mode
   :ensure t
