316a576549820005f010c5c113fe39074875b780
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-10-13 11:40:56 +0200

    boot up lsp in php-mode

--- lisp/init-php.el
+++ lisp/init-php.el
@@ -1,11 +1,14 @@
 (defun setup-php ()
 			(setenv "GTAGSLABEL" "pygments")
+			(setq-local c-basic-offset 4)
+			(lsp)
 	)
 
 (use-package php-mode
 	:ensure t
 	:init
-	(add-hook 'php-mode-hook 'setup-php))
+	(add-hook 'php-mode-hook 'setup-php)
+	)
 
 
 (provide 'init-php)
