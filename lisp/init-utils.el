154f149cab78decb56342b21c122a22bf29915cd
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-12-04 09:08:08 +0100

    make rename current file and buffer cmd insert the current name

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -227,15 +227,18 @@ If N is nil, use `ivy-mode' to browse the `kill-ring'."
 	(kill-new (buffer-file-name)))
 
 
-(defun rename-file-and-buffer (new-name)
-	"Renames both current buffer and file it's visiting to NEW-NAME." (interactive "sNew name: ")
-	(let ((name (buffer-name))
-				(filename (buffer-file-name)))
+(defun rename-file-and-buffer ()
+	"Renames both current buffer and file it's visiting to NEW-NAME."
+	(interactive)
+	(let*  ((name (buffer-name))
+				 (filename (buffer-file-name))
+				 (new-name (read-from-minibuffer "New name: " name)))
 		(if (not filename)
 				(message "Buffer '%s' is not visiting a file!" name)
 			(if (get-buffer new-name)
 					(message "A buffer named '%s' already exists!" new-name)
-				(progn	 (rename-file filename new-name 1)	 (rename-buffer new-name)		 (set-visited-file-name new-name)		 (set-buffer-modified-p nil))))))
+				(progn	 (rename-file filename new-name 1)	 (rename-buffer new-name)		 (set-visited-file-name new-name)		 (set-buffer-modified-p nil))))
+		))
 
 ;; Never understood why Emacs doesn't have this function, either.
 ;;
