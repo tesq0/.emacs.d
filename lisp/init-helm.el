0fc5e774e6e97586d32654bf04f2d78496284da0
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-12-25 23:02:10 +0100

    allow mouse helm

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -55,7 +55,8 @@
 	 helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
 	 helm-scroll-amount 8 ; scroll 8 lines other window using M-<next>/M-<prior>
 	 helm-ff-file-name-history-use-recentf t
-	 helm-echo-input-in-header-line t)
+	 helm-echo-input-in-header-line t
+	 helm-allow-mouse t)
 	(setq helm-autoresize-max-height 0)
 	(setq helm-autoresize-min-height 40)
 	(helm-autoresize-mode 1)
