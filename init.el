bf961e6c205ee255c06ea68f117e328633cf6fea
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2020-11-18 21:14:24 +0100

    indentation

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
 (setq recenter-positions '(middle top bottom))                    ; recenter from the top instead of the middle
 ;; (put 'narrow-to-defun 'disabled nil)                    ; enable narrowing to function
 (when (fboundp 'winner-mode)                      ; when you can find 'winner-mode'
-	(winner-mode 1))                        ; activate winner mode
+  (winner-mode 1))                        ; activate winner mode
 ;;(setq enable-recursive-minibuffers t)                   ; use the minibuffer while using the minibuffer
 (setq echo-keystrokes 0.05)                     ; when to echo keystrokes
 ;;(setq frame-resize-pixelwise t)                     ; resize based on pixels to remove annoying gaps
@@ -34,14 +34,14 @@
 (setq make-backup-files t)
 (setq backup-inhibited nil)
 (setq backup-directory-alist
-			`((".*" . ,emacs-backup-dir)))
+      `((".*" . ,emacs-backup-dir)))
 
 (setq auto-save-default t)
 
 (setq auto-save-file-name-transforms
-			`((".*" ,emacs-tmp-dir t)))
+      `((".*" ,emacs-tmp-dir t)))
 (setq auto-save-list-file-prefix
-			emacs-tmp-dir)
+      emacs-tmp-dir)
 
 (setq case-fold-search nil)
 (setq initial-buffer-choice t)					; use scratchpad as default buffer when calling emacsclient
@@ -56,7 +56,7 @@
 
 ;; extra functions
 (add-to-list 'load-path
-						 (expand-file-name "defuns" user-emacs-directory))
+	     (expand-file-name "defuns" user-emacs-directory))
 
 
 ;; TRAMP
@@ -70,23 +70,23 @@
 ;; manually installed packages
 
 (defun add-to-loadpath-recursive (dir)
-	(let ((default-directory dir))
-		(normal-top-level-add-to-load-path '("."))
-		(normal-top-level-add-subdirs-to-load-path)))
+  (let ((default-directory dir))
+    (normal-top-level-add-to-load-path '("."))
+    (normal-top-level-add-subdirs-to-load-path)))
 
 (add-to-loadpath-recursive (expand-file-name "site-lisp" user-emacs-directory))
 
 (let ((nixos-lisp-path "/run/current-system/sw/share/emacs/site-lisp"))
-	(when (file-exists-p nixos-lisp-path)
-		(add-to-loadpath-recursive nixos-lisp-path)))
+  (when (file-exists-p nixos-lisp-path)
+    (add-to-loadpath-recursive nixos-lisp-path)))
 
 (add-to-list 'load-path
-						 (expand-file-name "lisp" user-emacs-directory))
+	     (expand-file-name "lisp" user-emacs-directory))
 
 (if (boundp 'constants) (require 'constants))
 
 (defgroup init nil
-	"Init packages config")
+  "Init packages config")
 
 (require 'init-package)
 (require 'init-const)
@@ -145,38 +145,38 @@
 
 ;; ERC client
 (after-load 'erc-backend
-	(progn
-		(require 'erc-sasl)
-		;; e.g. irc\\.freenode\\.net, or .* for any host
-		(add-to-list 'erc-sasl-server-regexp-list "irc\\.freenode\\.net")))
+  (progn
+    (require 'erc-sasl)
+    ;; e.g. irc\\.freenode\\.net, or .* for any host
+    (add-to-list 'erc-sasl-server-regexp-list "irc\\.freenode\\.net")))
 
 
 ;; auto revert mode
 (use-package autorevert
-	:defer t
-	:diminish auto-revert-mode)
+  :defer t
+  :diminish auto-revert-mode)
 
 (setq debug-on-error nil)
 
 ;; hint for bindings
 (use-package which-key
-	:ensure t
-	:demand t
-	:diminish which-key-mode
-	:bind* (("C-c ?" . which-key-show-top-level))
-	:config
-	;; workaround for emacs 26
-	(if (version< emacs-version "26")
-			(message "Tracking stable Emacs")
-		(defalias 'display-buffer-in-major-side-window 'window--make-major-side-window))
-	;; turn on which key and add some names for default/common prefixes
-	(which-key-enable-god-mode-support)
-	(which-key-mode))
+  :ensure t
+  :demand t
+  :diminish which-key-mode
+  :bind* (("C-c ?" . which-key-show-top-level))
+  :config
+  ;; workaround for emacs 26
+  (if (version< emacs-version "26")
+      (message "Tracking stable Emacs")
+    (defalias 'display-buffer-in-major-side-window 'window--make-major-side-window))
+  ;; turn on which key and add some names for default/common prefixes
+  (which-key-enable-god-mode-support)
+  (which-key-mode))
 
 
 (use-package restart-emacs
-	:ensure t
-	:bind* (("C-x C" . restart-emacs)))
+  :ensure t
+  :bind* (("C-x C" . restart-emacs)))
 
 
 
@@ -186,24 +186,24 @@
 (defvar point-before-jump nil)
 
 (defun before-jump ()
-	"Save current POINT before jumping to another location."
-	(interactive)
-	(setq point-before-jump (point-at-bol)))
+  "Save current POINT before jumping to another location."
+  (interactive)
+  (setq point-before-jump (point-at-bol)))
 
 (defun my/hl-line ()
-	"Highlight line at point."
-	(interactive)
-	(when (not (eq (point-at-bol) point-before-jump) ) ;; if at different point than before the jump
-		(isearch-highlight (point-at-bol) (point-at-eol))
-		(when mikus-flash-timer
-			(cancel-timer mikus-flash-timer))
-		(setq mikus-flash-timer
-					(run-at-time 0.5 nil 'isearch-dehighlight))))
+  "Highlight line at point."
+  (interactive)
+  (when (not (eq (point-at-bol) point-before-jump) ) ;; if at different point than before the jump
+    (isearch-highlight (point-at-bol) (point-at-eol))
+    (when mikus-flash-timer
+      (cancel-timer mikus-flash-timer))
+    (setq mikus-flash-timer
+	  (run-at-time 0.5 nil 'isearch-dehighlight))))
 
 (defun try-hl-line-after-file-opened (buffer &rest args)
-	"Try to highlight line after switching to a BUFFER."
-	(when buffer
-		(my/hl-line)))
+  "Try to highlight line after switching to a BUFFER."
+  (when buffer
+    (my/hl-line)))
 
 (advice-add 'switch-to-buffer :after 'try-hl-line-after-file-opened )
 (advice-add 'evil-jump-backward :before 'before-jump)
@@ -225,30 +225,30 @@
 
 
 (use-package editorconfig
-	:ensure t
-	:config (editorconfig-mode 1))
+  :ensure t
+  :config (editorconfig-mode 1))
 
 (use-package elec-pair
-	:ensure t
-	:config (electric-pair-mode t))
+  :ensure t
+  :config (electric-pair-mode t))
 
 (use-package darkroom
-	:ensure t
-	:init
-	(progn
-		(require 'darkroom)
-		(setq darkroom-text-scale-increase 1)
-		(general-define-key
-		 "<C-f11>" 'darkroom-mode)
-		)
-	)
+  :ensure t
+  :init
+  (progn
+    (require 'darkroom)
+    (setq darkroom-text-scale-increase 1)
+    (general-define-key
+     "<C-f11>" 'darkroom-mode)
+    )
+  )
 
 ;; (use-package company-tern
 ;; 	:ensure t
 ;; 	)
 
 (use-package rainbow-mode
-	:ensure t)
+  :ensure t)
 
 ;; defuns
 
@@ -306,116 +306,116 @@
 
 ;; Annoying undo tree keybindings
 (general-unbind
- :keymaps 'undo-tree-map
- "C-_" nil
- "C-/" nil
- "C-?" nil
- "M-_" nil)
+  :keymaps 'undo-tree-map
+  "C-_" nil
+  "C-/" nil
+  "C-?" nil
+  "M-_" nil)
 
 (general-unbind
- "C-_" nil
- "C-/" nil
- "C-?" nil
- "M-_" nil)
+  "C-_" nil
+  "C-/" nil
+  "C-?" nil
+  "M-_" nil)
 
 (after-load 'imenu
-	(setq imenu-auto-rescan t))
+  (setq imenu-auto-rescan t))
 
 (require 'csharp-hs-forward-sexp)
 
 (use-package markdown-mode
-	:ensure t)
+  :ensure t)
 
 (use-package ahk-mode
-	:ensure t)
+  :ensure t)
 
 (use-package mmm-mode
-	:commands mmm-mode
-	:config
-	(use-package mmm-auto
-		:ensure nil))
+  :commands mmm-mode
+  :config
+  (use-package mmm-auto
+    :ensure nil))
 
 (use-package sonic-pi
-	:ensure t
-	:init
-	(progn
-		(setq sonic-pi-server-bin "App\\Sonic Pi\\app\\server\\ruby\\bin\\sonic-pi-server.rb")
-		(setq sonic-pi-compile-extensions-bin "App\\Sonic Pi\\app\\server\\ruby\\bin\\compile-extensions.rb")
-		(setq sonic-pi-path "D:\\Programs\\SonicPiPortable\\")))
+  :ensure t
+  :init
+  (progn
+    (setq sonic-pi-server-bin "App\\Sonic Pi\\app\\server\\ruby\\bin\\sonic-pi-server.rb")
+    (setq sonic-pi-compile-extensions-bin "App\\Sonic Pi\\app\\server\\ruby\\bin\\compile-extensions.rb")
+    (setq sonic-pi-path "D:\\Programs\\SonicPiPortable\\")))
 
 (use-package dockerfile-mode
-	:ensure t)
+  :ensure t)
 
 (use-package rainbow-delimiters
-	:ensure t
-	:init
-	(add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 (use-package aweshell
-	:quelpa (aweshell :fetcher github :repo "manateelazycat/aweshell")
-	:init
-	(progn
-		(require 'aweshell)
+  :quelpa (aweshell :fetcher github :repo "manateelazycat/aweshell")
+  :init
+  (progn
+    (require 'aweshell)
 
-		(defun load-aweshell-company-bindings ()
-			(general-unbind company-active-map "RET"))
-		
-		(defun unload-aweshell-company-bindings ()
-			(message "UNLOAD")
-			(general-define-key :keymaps 'company-active-map
-													"RET" 'company-complete-selection))
+    (defun load-aweshell-company-bindings ()
+      (general-unbind company-active-map "RET"))
+    
+    (defun unload-aweshell-company-bindings ()
+      (message "UNLOAD")
+      (general-define-key :keymaps 'company-active-map
+			  "RET" 'company-complete-selection))
 
-		(defun handle-loadin-eshell-keybindings ()
-			(message "Handle loading eshell keybindings")
-			(if (eshell-mode)
-																		(load-aweshell-company-bindings)
-				(unload-aweshell-company-bindings)))
-	
-		(defun setup-aweshell-keybindings ()
-			(load-aweshell-company-bindings)
-			(general-define-key
-			 :keymap 'eshell-mode-map
-			 "C-n" 'eshell-next-input
-			 "C-p" 'eshell-previous-input
-			 "C-c c" 'aweshell-clear-buffer
-			 "C-c n" 'aweshell-next
-			 "C-c p" 'aweshell-prev
-			 "C-c s" 'aweshell-sudo-toggle
-			 "C-c x" 'aweshell-new))
-		)
-	)
+    (defun handle-loadin-eshell-keybindings ()
+      (message "Handle loading eshell keybindings")
+      (if (eshell-mode)
+	  (load-aweshell-company-bindings)
+	(unload-aweshell-company-bindings)))
+    
+    (defun setup-aweshell-keybindings ()
+      (load-aweshell-company-bindings)
+      (general-define-key
+       :keymap 'eshell-mode-map
+       "C-n" 'eshell-next-input
+       "C-p" 'eshell-previous-input
+       "C-c c" 'aweshell-clear-buffer
+       "C-c n" 'aweshell-next
+       "C-c p" 'aweshell-prev
+       "C-c s" 'aweshell-sudo-toggle
+       "C-c x" 'aweshell-new))
+    )
+  )
 
 (use-package yaml-mode
-	:ensure t)
+  :ensure t)
 
 (use-package firestarter
-	:ensure t
-	:init
-	(firestarter-mode))
+  :ensure t
+  :init
+  (firestarter-mode))
 
 (use-package pomodoro
-	:ensure t
-	:init
-	(progn
-		(setq pomodoro-sound-player "mpv"
-					pomodoro-desktop-notification t))
-	:config
-	(pomodoro-add-to-mode-line))
+  :ensure t
+  :init
+  (progn
+    (setq pomodoro-sound-player "mpv"
+	  pomodoro-desktop-notification t))
+  :config
+  (pomodoro-add-to-mode-line))
 
 (use-package edit-server
-	:ensure t)
+  :ensure t)
 
 (use-package direnv
- :config
- (direnv-mode))
+  :config
+  (direnv-mode))
 
 ;; snippets
 (use-package yasnippet
-	:ensure t
-	:init (progn
-					(add-to-list 'company-backends #'company-yasnippet))
-	:config
-	(setq yas-snippet-dirs '("~/.emacs.d/snippets")))
+  :ensure t
+  :init (progn
+	  (add-to-list 'company-backends #'company-yasnippet))
+  :config
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets")))
 
 (setq custom-file (concat user-emacs-directory "custom-set-variables.el"))
 (load custom-file 'noerror)
