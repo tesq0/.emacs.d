bff241a9fcff732d9382d0c6c905e602bd9395ba
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2020-11-18 19:24:51 +0100

    lsp in C

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -4,36 +4,20 @@
   "Options for C language setup"
   :group 'init)
 
-(defcustom irony-disabled-modes '()
-	"List of disabled irony modes."
+(defcustom c-langserver-disabled-modes '()
+	"List of c langserver disabled modes."
 	:group 'c-options
 	:type '(set symbol))
 
-(use-package irony
-	:ensure t
-	:init
-	(progn
+(after-load 'lsp-mode
+	(defun enable-irony-mode ()
+		(and
+		 (not (memq major-mode c-langserver-disabled-modes))
+		 (lsp)))
 
-		(defun enable-irony-mode ()
-			(and
-			 (not (memq major-mode irony-disabled-modes))
-			 (irony-mode)))
-
-		(setq irony--server-executable (concat (getenv "HOME") "/.nix-profile/bin/irony-server"))
-		(add-hook 'c++-mode-hook 'irony-mode)
-		(add-hook 'c-mode-hook 'enable-irony-mode)
-		(add-hook 'objc-mode-hook 'irony-mode)
-		(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)))
-
-(use-package flycheck-irony
-	:after irony
-	:ensure t
-	:init
-	(flycheck-irony-setup))
-
-(use-package irony-eldoc
-	:after irony
-	:ensure t)
+	(add-hook 'c++-mode-hook 'irony-mode)
+	(add-hook 'c-mode-hook 'enable-irony-mode)
+	(add-hook 'objc-mode-hook 'irony-mode))
 
 (defun init-c-style()
 	"Define my own indenting style for C."
@@ -49,18 +33,9 @@
 									(arglist-cont-nonempty . 0)
 									(arglist-intro . +)
 									(arglist-close . c-lineup-close-paren))))
-	(add-to-list 'c-default-style '(c-mode . "mikus-c"))
-	)
+	(add-to-list 'c-default-style '(c-mode . "mikus-c")))
 
 (after-load 'cc-mode
-	(init-c-style)
-	(add-hook 'c-mode-hook #'irony-eldoc))
-
-(defun irony-iotask-ectx-call-callback (ectx result)
-  (let ((cb-buffer (irony-iotask-ectx-schedule-buffer ectx)))
-    (when (buffer-live-p cb-buffer)
-      (with-demoted-errors "Irony I/O task: error in callback: %S"
-        (with-current-buffer cb-buffer
-          (funcall (irony-iotask-ectx-callback ectx) result))))))
+	(init-c-style))
 
 (provide 'init-c)
