a0091b46abd59a42b97e5cc4e1047cd5bbbd5b56
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2021-02-03 22:30:20 +0100

    rm old code

--- lisp/init-nix.el
+++ lisp/init-nix.el
@@ -4,24 +4,8 @@
   (defun init-nix-mode ()
     (setq-local company-backends '(company-yasnippet company-files (company-dabbrev-code company-capf company-keywords) company-dabbrev)))
   (add-hook 'nix-mode-hook 'init-nix-mode)
-  (setq nix-indent-function 'nix-indent-line))
-
-(defun fix-nix-options ()
-  (interactive)
-
-  ;; (setq nixos-options-json-file
-  ;; 			(let* ((cmd
-  ;; 							"export NIXPKGS_ALLOW_UNFREE=1; nix-build -Q --no-out-link '<nixpkgs/nixos/release.nix>' -A options 2>/dev/null")
-  ;; 						 (dir (replace-regexp-in-string "\n\\'" ""
-  ;; 																						(shell-command-to-string cmd))))
-  ;; 				(expand-file-name "share/doc/nixos/options.json" dir)))
-
-  ;; (setq nixos-options
-  ;; 			(if (file-exists-p nixos-options-json-file)
-  ;; 					(let* ((json-key-type 'string)
-  ;; 								 (raw-options (json-read-file nixos-options-json-file)))
-  ;; 						(mapcar 'nixos-options--make-alist raw-options))
-  ;; 				(message "Warning: Cannot find nixos option file.")))
+  (setq nix-indent-function 'nix-indent-line)
   )
+
 
 (provide 'init-nix)
