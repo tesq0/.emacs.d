2562e8ee944edf1c76bdba82e5c85485153ce74e
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2021-02-14 21:04:11 +0100

    rm static pretteir pkg

--- site-lisp/prettier-0.7.0/prettier-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "prettier" "0.7.0" "Code formatting with Prettier" '((emacs "24.4") (iter2 "0.9") (nvm "0.2")) :keywords '("convenience" "languages" "files") :authors '(("Julian Scheid" . "julians37@gmail.com")) :maintainer '("Julian Scheid" . "julians37@gmail.com") :url "https://github.com/jscheid/prettier.el")
