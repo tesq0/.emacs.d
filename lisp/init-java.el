06ddf4f199d270857bd6542e9f5e80c35d993ade
Mikołaj Gałkowski <mikolaj.galkowski@vox.pl>
2021-09-06 11:57:20 +0200

    fixup! enable lsp in java mode

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -18,8 +18,7 @@
   ;; (advice-remove 'lsp-java-boot--find-tools-jar #'lsp-java-boot--find-tools-jar-nixos)
   (setq lsp-java-format-enabled nil))
 
-(after-load 'lsp-java
-  (add-hook 'java-mode-hook 'lsp))
+(add-hook 'java-mode-hook 'lsp)
 
 (use-package groovy-mode
   :ensure t)
