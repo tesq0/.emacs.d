f59a3b25c5d23e2f186a57c86e015a6128fe4f61
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2021-11-14 13:10:32 +0100

    speedup startup time

--- init.el
+++ init.el
@@ -111,6 +111,14 @@
 (cl-letf (((symbol-function 'define-obsolete-function-alias) #'defalias))
   (benchmark-init/show-durations-tabulated))
 
+;; Minimize garbage collection during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 23))))
+
 (defgroup init nil
   "Init packages config")
 
